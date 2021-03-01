65176b7d2205dc60543d60158f136c4e9116cb81
Mikhael6363 <mikhael.6363@gmail.com>
2021-03-01 21:39:35 +0200

    Add Carousel Data Base

---
+++ src/components/Carousel/CarouselDB/index.jsx
@@ -0,0 +1,39 @@
+const carouselData = [
+  {
+    src:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-cosmic-reef-2400x1200.jpg?t=tn700',
+    title: 'The Cosmic Reef: NGC 2014 and NGC 2020',
+    description:
+      'Hubble reveals a colorful landscape of iridescent gases, streamers of dust, and brilliant, newborn stars in the giant red nebula NGC 2014. The blue ring of glowing oxygen at left is the neighboring nebula NGC 2020, formed by a torrential gaseous outflow from a lone, massive, super-hot star at its center.',
+  },
+  {
+    src:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-lagoon-nebula.jpg?t=tn700',
+    title: 'Lagoon Nebula',
+    description:
+      'The Lagoon Nebula, a vast stellar nursery located 4,000 light-years away, provides an extraordinary tapestry of star birth.',
+  },
+  {
+    src:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-hh24.jpg?t=tn700',
+    title: 'HH24 "light saber"',
+    description:
+      'The newborn star in Herbig-Haro 24 (HH 24) is hidden from direct view, but jets blasting out along its rotation axis create a series of glowing shock fronts, revealing the new star’s presence.',
+  },
+  {
+    src:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-eagle-nebula.jpg?t=tn700',
+    title: 'Eagle Nebula',
+    description:
+      'Stars are being born deep inside the so-called Pillars of Creation, which are made of cold hydrogen gas laced with dust.',
+  },
+  {
+    src:
+      'https://hubblesite.org/files/live/sites/hubble/files/home/hubble-30th-anniversary/images/_images/hubble_30th_images/hubble-30th-saturn.jpg?t=tn700',
+    title: 'Saturn',
+    description:
+      'Hubble captured exquisite views of Saturn’s ring system and atmospheric details that once could only be provided by spacecraft visiting the distant world.',
+  },
+];
+
+export default carouselData;
